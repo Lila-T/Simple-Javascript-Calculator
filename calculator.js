48d5bb0def31f15c7a4efbd131e96fd905a93d51
Lila-T <42620211+Lila-T@users.noreply.github.com>
2018-08-30 23:41:59 +0430

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,66 @@
+var resultBox = document.getElementById('resultBox'),
+	screenBox = document.getElementById('screenBox'),
+	inputArray = [],
+	opratorArray = [];
+	
+init();
+
+function init(){
+	screenBox.focus();
+}	
+
+function clickOnNumber(number){
+	screenBox.value += number;
+}
+
+function clickOnDote(){
+	screenBox.value += '.';
+}
+function clearResultBox(){
+	screenBox.value = '';
+	resultBox.value = '';
+	inputArray = [];
+	opratorArray = [];
+}
+
+function backSpace(){
+	screenBox.value = screenBox.value.slice(0,-1);
+}
+
+function onScreenBoxKeydown(){
+	return screenBox.value;
+}
+function clickOnOprator(oprator){
+	
+	if(opratorArray.length == 0){
+		inputArray.push(screenBox.value);
+	}else{
+		resultNumber = screenBox.value.substring(screenBox.value.lastIndexOf(opratorArray[0])+1 , screenBox.value.length);
+		if(resultNumber == "") return;
+		inputArray.push(resultNumber);
+	}
+	equal();
+	opratorArray =[];
+	opratorArray.push(oprator);
+	if( oprator == "=") {
+		screenBox.value = '';
+		opratorArray= [];
+	}else{
+		screenBox.value += oprator;
+	}
+	
+}
+
+
+
+function equal(){
+	if(opratorArray.length == 0 ) {
+		resultBox.value = inputArray[0];
+	}else{
+		resultBox.value = eval(resultBox.value + opratorArray[opratorArray.length - 1] + inputArray[inputArray.length - 1]);
+		inputArray= [];
+		inputArray.push(resultBox.value);
+	}
+	
+
+}
